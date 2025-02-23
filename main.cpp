803104fe4a64d0f4a7a51dc79c6e83b9aef1885e
Nishad <thanveer2893@gmail.com>
2025-02-23 21:07:51 +0530

    feat: parser class,parser func for insert query,insert query struct

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include<limits>
 #include <unordered_set>
 
 std::unordered_set<std::string> SQL_KEYWORDS= {
@@ -22,21 +23,26 @@ typedef struct{
 }TOKEN;
 
 
+typedef struct{
+    std::string table_name;
+    std::string values;
+}S_INSERT_QUERY;
+
+
 class TOKENIZER{
-private:
-    int _pos = 0;
+
+    private:
+    size_t _pos = 0;
     std::string _query;
     std::vector<TOKEN> _token_vec;
 
-public:
+    public:
     TOKENIZER(std::string& query) : _query(query){
         tokenize();
     }
 
-    void display(){
-        for(auto& tkn : _token_vec){
-            std::cout<<"Token:"<<tkn.word<<'\n';
-        }
+    std::vector<TOKEN> get_token_vec(){
+        return _token_vec;
     }
 
     void tokenize(){
@@ -82,12 +88,111 @@ public:
     }
 };
 
+class PARSER{
+    private:
+    size_t _pos=0;
+    S_INSERT_QUERY insert_query;
+    std::vector<TOKEN> _t_v;
+
+    public:
+    PARSER(std::vector<TOKEN> temp ) : _t_v(temp){
+        parser_fn();
+    } 
+    
+    void parser_fn(){
+
+        if(_t_v[_pos].word == "INSERT" && _t_v[_pos].tk_type == TOKENTYPE::KEYWORD ){
+            insert_parse();
+        }
+        else if(_t_v[_pos].word == "SELECT" && _t_v[_pos].tk_type == TOKENTYPE::KEYWORD ){
+            select_parse();
+        }
+        else if(_t_v[_pos].word == "UPDATE" && _t_v[_pos].tk_type == TOKENTYPE::KEYWORD ){
+            update_parse();
+        }
+        else if(_t_v[_pos].word == "DELETE" && _t_v[_pos].tk_type == TOKENTYPE::KEYWORD ){
+            delete_parse();
+        }
+
+    }
+
+    void insert_parse(){
+        if(_t_v[_pos].word != "INSERT"){
+            throw std::runtime_error("SYNTAX ERROR : expected 'INSERT' ");
+        }
+        _pos++;
+
+        if(_t_v[_pos].word != "INTO"){
+            throw std::runtime_error("SYNTAX ERROR : expected 'INTO' ");
+        }
+        _pos++;
+
+        if(_t_v[_pos].tk_type != TOKENTYPE::IDENTIFIER){
+            throw std::runtime_error("SYNTAX ERROR : expected table name ");
+
+        }
+        insert_query.table_name = _t_v[_pos].word;
+        _pos++;
+
+        if(_t_v[_pos].word != "VALUES"){
+            throw std::runtime_error("SYNTAX ERROR : expected 'VALUES' ");
+        }
+        _pos++;
+
+        if(_t_v[_pos].word != "("){
+            throw std::runtime_error("SYNTAX ERROR : expected '(' ");
+        }
+        _pos++;
+
+        std::string values;
+        while(_pos < _t_v.size() && _t_v[_pos].word != ")"){
+            values += _t_v[_pos].word;
+            _pos++;
+        }
+
+        if(_t_v[_pos].word != ")"){
+            throw std::runtime_error("SYNTAX ERROR : expected ')' ");
+        }
+        _pos++;
+
+        if(_t_v[_pos].word != ";"){
+            throw std::runtime_error("SYNTAX ERROR : expected ';' ");
+        }
+        _pos++;
+
+        insert_query.values = values;        
+    }
+     
+    void select_parse(){
+    }
+
+    void update_parse(){
+
+    }
+
+    void delete_parse(){
+
+    }
+};
+
+
+class QUERY_ENGINE{
+    private:
+    std::string query;
+
+    public:
+    QUERY_ENGINE(std::string temp) : query(temp){
+        TOKENIZER tk_obj(query);
+        PARSER pars_obj(tk_obj.get_token_vec());
+    }
+};
 
 int main(){
     std::string query;
     while(true){
         std::cout<<"dbl>";
-        std::cin>>query;
+        std::cout.flush();
+        std::getline(std::cin,query);
 
         if(query == ".exit"){
             std::cout<<"Program exited";
@@ -95,7 +200,17 @@ int main(){
         }
 
         else{
-            TOKENIZER tokenizer_obj(query);
+            try{
+            QUERY_ENGINE QE(query);
+            }
+            catch(const std::exception& e){
+                std::cout<<"Error:"<<e.what()<<"\n";
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                query="";
+                continue;
+
+            }
         }
         
     }
